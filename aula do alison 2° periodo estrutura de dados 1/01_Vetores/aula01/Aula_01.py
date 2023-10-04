577f41d5e5c7742709a955cfd267868e67c746ae
Nathan Antonio <125812170+flowingTunic@users.noreply.github.com>
2023-10-03 21:45:31 -0300

    Revert "at1"
    This reverts commit 36796e70332a99219dcbed028347830b54a3022f.

--- aula do alison 2° periodo estrutura de dados 1/01_Vetores/aula01/Aula_01.py
+++
@@ -1,6 +0,0 @@
-nomes=[]
-for i in range (10):
-    tmp = input(f"digite o nome {i} : ")
-    nomes.append(tmp) #adiciona valor na lista 
-
-print(nomes)
--- aula do alison 2° periodo estrutura de dados 1/aula_31_08_2023/exemplo02json.py
+++
@@ -1,8 +0,0 @@
-import json
-#"r" - abrir para leitura
-#"w" - abrir para escrita (sobreescrever)
-#"a" - abrir para escrita (anexa no fim)
-with open("aula_31-08_2023\dados.json", "r") as arquivo:
-    registros = json.load(arquivo)
-
-print(registros)
